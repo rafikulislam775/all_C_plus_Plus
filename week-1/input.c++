9d83e09d619620f0e53c4e705d17b6b014a0a287
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-12-17 08:46:35 +0900

    how to apply  input and output in c++

---
+++ week-1/input.c++
@@ -0,0 +1,17 @@
+#include <iostream>
+//bara bar  std:: jen na kekha lage
+using namespace std;
+
+int main(){
+    int  a;
+    long long b;
+    //long long int  x = (long long int )(a)
+    // input neyar jnn 
+    cin >> a >>b;
+    cout << a <<" "<<b <<endl;
+    char c;
+    cin>>c;
+    int asciiValue =c; // ba int(c)  also same
+    cout<<asciiValue <<"\n";
+
+}
---
+++ week-1/output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // std:: cout<<"Running"; print korar jnn
+
+    std:: cout<<"Running";
+    int a = 10;
+    long long b = 100000;
+    float c = 1.5;
+    double d =1.500000002;
+    char e = 'a';
+    std:: cout<< a;
+    //new line er jnn "\n " ba std::endl;
+    std::cout<<"that is a "<<a <<"\n" <<b <<"\n" <<"that is awesome";
+
+    return 0;
+}
